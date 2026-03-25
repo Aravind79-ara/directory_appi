851bc1f22b6b12362c13e216a8f55549e72d041c
Aravind79-ara <aravindgonela722@gmail.com>
2026-03-25 22:09:52 +0530

    nsssd

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { HashRouter,Route,Routes} from 'react-router-dom';
+import { HashRouter,Route,Routes,Router} from 'react-router-dom';
 import Details from './componentss/Details';
 import User from './componentss/User';
 
